511c81bf846bd682ad88e2f91bb6235b7767324d
VINITH S <85937627+realvineeths@users.noreply.github.com>
2023-08-05 14:02:32 +0530

    producer minor changes

--- producer/producerindex.js
+++ producer/producerindex.js
@@ -7,22 +7,17 @@ require('dotenv').config({path:'.././.env'});
 
 
 app.use(cors());
-var PORT1 = 6000;
-// var PORT = 5000;
 var PORT = process.env.BROKERPORT;
-
-var TESTTOPIC = 'two';
-var MES = "Heyyy f U";
 
 var envtt = process.argv[2].split('=');
 var envport = process.argv[3].split('=');
 
-TESTTOPIC=envtt;
+var TESTTOPIC=envtt;
 var message = process.argv[4];
-MES = message;
-PORT1=parseInt(envport[1]);
-console.log(envtt[1], parseInt(envport[1]));
-console.log(MES);
+var MES = message;
+var PORT1=parseInt(envport[1]);
+// console.log(envtt[1], parseInt(envport[1]));
+// console.log(MES);
 
 
 app.listen(PORT1, (req, res) => {
